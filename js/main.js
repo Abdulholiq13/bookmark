74deb827a29c5eaa61e5b1cdf2b67109d93b4420
Abdulholiq13 <abdulholiq571@gmail.com>
2023-07-28 09:42:17 +0500

    Rafactor modifier classes into modifier object

--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
-const elsTabsItem = document.querySelectorAll('.tabs__item');
-const elsTabsPanel = document.querySelectorAll('.tabs__panel');
-const elsTabLink = document.querySelectorAll('.js-tab-link');
+const modifiers = {
+  tabItemActive: "tabs__item--active",
+  tabPAnelActive: "tabs__panel--active",
+  accordionItemOpen: "accordion__item--open",
+};
 
-const elsAccordionItem = document.querySelectorAll('.accordion__item');
-const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
+const elsTabsItem = document.querySelectorAll(".tabs__item");
+const elsTabsPanel = document.querySelectorAll(".tabs__panel");
+const elsTabLink = document.querySelectorAll(".js-tab-link");
+
+const elsAccordionItem = document.querySelectorAll(".accordion__item");
+const elsAccordionItemToggler = document.querySelectorAll(
+  ".accordion__item-toggler"
+);
 
 function deactivateTabItem() {
   elsTabsItem.forEach(function (elsTabsItem) {
-    elsTabsItem.classList.remove('tabs__item--active');
+    elsTabsItem.classList.remove(modifiers.tabItemActive);
   });
 }
 
 function deactivateTabPanels() {
   elsTabsPanel.forEach(function (elTabsPanel) {
-    elTabsPanel.classList.remove('tabs__panel--active');
+    elTabsPanel.classList.remove(modifiers.tabPAnelActive);
   });
 }
 
 function closeAccordionItems() {
   elsAccordionItem.forEach(function (elAccordionItem) {
-    elAccordionItem.classList.remove('accordion__item--open');
+    elAccordionItem.classList.remove(modifiers.accordionItemOpen);
   });
 }
 
 elsTabLink.forEach(function (elTabLink) {
-  elTabLink.addEventListener('click', function (evt) {
+  elTabLink.addEventListener("click", function (evt) {
     // Prevent page move
     evt.preventDefault();
 
@@ -32,7 +40,7 @@ elsTabLink.forEach(function (elTabLink) {
     deactivateTabItem();
 
     // Add active class to current tabs__item
-    elTabLink.parentElement.classList.add('tabs__item--active');
+    elTabLink.parentElement.classList.add(modifiers.tabItemActive);
 
     // Remove active class from tabs__panel elements
     deactivateTabPanels();
@@ -40,14 +48,16 @@ elsTabLink.forEach(function (elTabLink) {
     // Show active tab panel
     // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
     const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
-    elTargetPanel.classList.add('tabs__panel--active')
+    elTargetPanel.classList.add(modifiers.tabPAnelActive);
   });
 });
 
 elsAccordionItemToggler.forEach(function (elAccordionItemToggler) {
-  elAccordionItemToggler.addEventListener('click', function () {
+  elAccordionItemToggler.addEventListener("click", function () {
     closeAccordionItems();
 
-    elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
+    elAccordionItemToggler
+      .closest(".accordion__item")
+      .classList.add("accordion__item--open");
   });
 });
